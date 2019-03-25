13aa6645000763132de38a29019894d2e23a1dcf
alswl6083 <48279215+alswl6083@users.noreply.github.com>
2019-03-25 23:59:50 +0900

    Add files via upload

---
+++ 3주차/client.py
@@ -0,0 +1,38 @@
+﻿import socket 
+import argparse 
+import glob 
+import sys 
+ 
+def run(host, port, f_name): 
+     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: 
+         s.connect(('127.0.0.1', 8888)) 
+         filename = f_name
+         s.sendall(filename.encode()) 
+         data = s.recv(1024)
+         print("file name : ", filename)
+         print("contents of file : ", data.decode()) 
+
+         if not data: 
+             print("failed to find file.") 
+             return 
+
+         with open(f_name,'wb') as f: 
+             try: 
+                 f.write(data) 
+                 data = s.recv(1024) 
+             except Exception as e: 
+                 print(e) 
+
+         print("Transfer completed.")  
+         print("List of files in the current directory : ") 
+         file_list = glob.glob('*') 
+         print(file_list) 
+ 
+ 
+if __name__ == '__main__': 
+    parser = argparse.ArgumentParser(description="Echo client -p port -i host -f f_name") 
+    parser.add_argument('-p', help="port_number", required=True) 
+    parser.add_argument('-i', help="host_name", required=True) 
+    parser.add_argument('-f', help="file_name", required=True) 
+    args = parser.parse_args() 
+    run(host=args.i, port=int(args.p), f_name = args.f) 
---
+++ 3주차/server.py
@@ -0,0 +1,38 @@
+﻿import socket 
+import argparse 
+import os 
+import glob
+ 
+def run_server(port=8888,file_dir = 0): 
+      host = '127.0.0.1' 
+      with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: 
+        s.bind((host,port)) 
+        s.listen(1) 
+        conn, addr = s.accept() 
+        filename = conn.recv(1024) 
+        filename = filename.decode() 
+        dir = file_dir + filename
+        print("file name : ", filename)
+        print("size : " , os.path.getsize(dir)) 
+
+        with open(file_dir + filename, 'rb') as f: 
+            try: 
+                data = f.read(1024) 
+                conn.sendall(data) 
+                print("Transfer completed.") 
+            except Exception as e: 
+                print(e) 
+        conn.close() 
+ 
+ 
+if __name__ == '__main__': 
+
+    print("List of files in the current directory : ") 
+    file_list = glob.glob('*') 
+    print(file_list)
+
+    parser = argparse.ArgumentParser(description="Echo server -p port -d file_dir") 
+    parser.add_argument('-p',help="port_number",required=True) 
+    parser.add_argument('-d',help="directory",required=True) 
+    args = parser.parse_args() 
+    run_server(port=int(args.p) , file_dir=args.d) 
